1166534fbfa1c502cde2640a4d447d182ef6dd18
Artem Hubar <hubar.artem@gmail.com>
2024-03-28 00:50:29 +0200

    add main

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,17 @@
 package org.example;
 
+import org.example.bonuses.BonusAdditionalCube;
+import org.example.bonuses.BonusBehavior;
+import org.example.bonuses.BonusStatic;
+import org.example.characters.Agility;
+import org.example.characters.Intellect;
+import org.example.entity.Advantage;
+import org.example.entity.Person;
+import org.example.characters.Character;
+import org.example.payload.GamePayload;
+
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -9,5 +21,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Character checkingCharacter = new Intellect(20);
+
+        ArrayList<Character> charPerson = new ArrayList<>();
+        ArrayList<BonusBehavior> bonuses = new ArrayList<>();
+        bonuses.add(new BonusStatic(3));
+        bonuses.add(new BonusAdditionalCube(4));
+        charPerson.add(new Intellect(15));
+        charPerson.add(new Agility(14));
+        Person person = new Person("Dratar",charPerson, new Advantage(true), bonuses);
+        GamePayload gamePayload = new GamePayload(checkingCharacter, person);
+        gamePayload.play();
     }
 }
